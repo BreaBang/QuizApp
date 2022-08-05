3794aa67380cbddf5730fc613c9a0ffdde8ddbc9
breabang <breanna.bang@gmail.com>
2022-08-05 09:23:35 -0600

    file updates

---
+++ app.js
@@ -0,0 +1,98 @@
+const questionDisplay = document.querySelector('#questions')
+const answerDisplay = document.querySelector('#answer')
+
+const questions = [
+    {
+        id: 0,
+        text: "Pick a vacation destination:",
+        answers: [
+            {
+                text: "New York",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Japan",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "China",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "London",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+        ]
+    },
+    {
+        id: 1,
+        text: "Pick color:",
+        answers: [
+            {
+                text: "Red",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Blue",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Green",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Yello",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+        ]
+
+    },
+    {
+        id: 3,
+        text: "Pick some food:",
+        answers: [
+            {
+                text: "Pasta",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Ice Cream",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Dumplings",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+            {
+                text: "Pizza",
+                image: "/Images/placeholder.jpg",
+                alt: "Photo of the Empire State building during daytime",
+                credit: "Canva"
+            },
+        ]
+    },
+
+
+]
